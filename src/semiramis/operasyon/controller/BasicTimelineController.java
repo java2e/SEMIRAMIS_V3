8945f4f48768893256f67276f1ed7f4d85f59c05
Ozgen <ozgn45@gmail.com>
2016-08-29 17:30:04 +0300

    Rapor Kronolojisi yazdır uyarı eklendi

--- src/semiramis/operasyon/controller/BasicTimelineController.java
+++ src/semiramis/operasyon/controller/BasicTimelineController.java
@@ -4,9 +4,12 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import org.primefaces.context.RequestContext;
 import org.primefaces.extensions.model.timeline.TimelineEvent;
@@ -28,56 +31,56 @@ public class BasicTimelineController implements Serializable {
 	private TimelineModel model;
 
 	TimelineEvent event;
-	
-	public static String DOSYA_CRH_JASPER ="dosya_chronology";
+
+	public static String DOSYA_CRH_JASPER = "dosya_chronology";
 
 	private String icraDosyaNo;
 	private List<Chronology> chronologies = new ArrayList<>();
 
 	private int icraDosyaID = 0;
-	
-	private ReportPublish publish = new ReportPublish(); 
+
+	private ReportPublish publish = new ReportPublish();
 
 	private Utils utils = new Utils();
 
 	public BasicTimelineController() {
-		//initialize();
+		// initialize();
 	}
 
-//	public void initialize() {
-//		model = new TimelineModel();
-//
-//		ArrayList<TimelineEvent> events = null;
-//		try {
-//			if (icraDosyaID == 0 && AktifBean.getIcraDosyaID() != 0) {
-//				events = utils.getAllTimeLineEventsFromID(AktifBean.getIcraDosyaID());
-//				chronologies = utils.getChronologyList(AktifBean.getIcraDosyaID());
-//			} else {
-//				events = utils.getAllTimeLineEventsFromID(icraDosyaID);
-//				chronologies = utils.getChronologyList(icraDosyaID);
-//
-//			}
-//
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		if (events != null) {
-//			if (events.size() > 0) {
-//				for (TimelineEvent timelineEvent : events) {
-//
-//					model.add(timelineEvent);
-//				}
-//			}
-//
-//		}
-//
-//	}
+	// public void initialize() {
+	// model = new TimelineModel();
+	//
+	// ArrayList<TimelineEvent> events = null;
+	// try {
+	// if (icraDosyaID == 0 && AktifBean.getIcraDosyaID() != 0) {
+	// events = utils.getAllTimeLineEventsFromID(AktifBean.getIcraDosyaID());
+	// chronologies = utils.getChronologyList(AktifBean.getIcraDosyaID());
+	// } else {
+	// events = utils.getAllTimeLineEventsFromID(icraDosyaID);
+	// chronologies = utils.getChronologyList(icraDosyaID);
+	//
+	// }
+	//
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// }
+	// if (events != null) {
+	// if (events.size() > 0) {
+	// for (TimelineEvent timelineEvent : events) {
+	//
+	// model.add(timelineEvent);
+	// }
+	// }
+	//
+	// }
+	//
+	// }
 
 	public void icraDosyaSec(int id) {
 		RequestContext.getCurrentInstance().execute("PF('dlgdetayliarama').hide()");
 		this.icraDosyaNo = AktifBean.getIcraDosyaNo();
 		icraDosyaID = AktifBean.getIcraDosyaID();
-		//initialize();
+		// initialize();
 		chronologies = utils.getChronologyList(icraDosyaID);
 
 	}
@@ -97,15 +100,26 @@ public class BasicTimelineController implements Serializable {
 	public void setChronologies(List<Chronology> chronologies) {
 		this.chronologies = chronologies;
 	}
-	
-	public void printExcell(){
-		if(chronologies.size()>0){
-			publish.getReportXLS(chronologies, DOSYA_CRH_JASPER );
+
+	public void printExcell() {
+		if (icraDosyaID != 0) {
+			if (chronologies.size() > 0) {
+				publish.getReportXLS(chronologies, DOSYA_CRH_JASPER);
+			}
+		} else {
+			FacesContext context = FacesContext.getCurrentInstance();
+			context.addMessage(null, new FacesMessage("Lütfen yazdırmak istediğiniz icra dosyasını şeçiniz!"));
 		}
 	}
-	public void printPDF(){
-		if(chronologies.size()>0){
-			publish.getReportPDF(chronologies,new HashMap<>() ,DOSYA_CRH_JASPER );
+
+	public void printPDF() {
+		if (icraDosyaID != 0) {
+			if (chronologies.size() > 0) {
+				publish.getReportPDF(chronologies, new HashMap<>(), DOSYA_CRH_JASPER);
+			}
+		} else {
+			FacesContext context = FacesContext.getCurrentInstance();
+			context.addMessage(null, new FacesMessage("Lütfen yazdırmak istediğiniz icra dosyasını şeçiniz!"));
 		}
 	}
 
